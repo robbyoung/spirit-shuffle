d40427b901773089c934429d4c1ce347dc587c69
Robert Young <robertyoung10101@gmail.com>
2023-02-11 13:30:16 +1300

    Add sass and update manifest

--- src/App.tsx
+++ src/App.tsx
@@ -8,24 +8,22 @@ import plant from './images/elements/plant.png'
 import beast from './images/elements/beast.png'
 
 import title from './images/si-title.png'
-import './App.css'
+import './App.scss'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={title} className="elemetn"></img>
-        <div>
-          <img src={sun} className="element spin" alt="logo" />
-          <img src={moon} className="element spin" alt="logo" />
-          <img src={fire} className="element spin" alt="logo" />
-          <img src={sky} className="element spin" alt="logo" />
-          <img src={water} className="element spin" alt="logo" />
-          <img src={mountain} className="element spin" alt="logo" />
-          <img src={plant} className="element spin" alt="logo" />
-          <img src={beast} className="element spin" alt="logo" />
-        </div>
-      </header>
+    <div className="app">
+      <img src={title} className="elemetn"></img>
+      <div>
+        <img src={sun} className="element spin" alt="logo" />
+        <img src={moon} className="element spin" alt="logo" />
+        <img src={fire} className="element spin" alt="logo" />
+        <img src={sky} className="element spin" alt="logo" />
+        <img src={water} className="element spin" alt="logo" />
+        <img src={mountain} className="element spin" alt="logo" />
+        <img src={plant} className="element spin" alt="logo" />
+        <img src={beast} className="element spin" alt="logo" />
+      </div>
     </div>
   )
 }
